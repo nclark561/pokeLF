84d6408d07c6b07fce9ba45f8434d627525ec809
Noah Clark <noahammon00@gmail.com>
2023-03-31 11:19:05 -0600

    added feature so that u can only view a users page if you are logged in as said user

--- src/components/UserPokemon.js
+++ src/components/UserPokemon.js
@@ -1,29 +1,49 @@
 import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import PokemonCard from "./PokemonCard.js"
 import "./UserPokemon.css"
 
 
 const UserPokemon = () => {
-    const pokemon = ['mewtwo-mega-y', 'zoroark']
-    const wishlist = ['scrafty', 'floette', 'shaymin-land']
+    const [pokemon, setPokemon] = useState()
+    const [wishlist, setWishlist] = useState()
 
-    return (
-        <div id="user-pokemon">
-            <h1 className="title">Your Pokemon</h1>
-            <div className="card-container">
-                {                
-                    pokemon.map(e => (<PokemonCard image={e} key={e}/>))
-                }              
+    const routeParams = useParams()
+
+    console.log(routeParams.id)
+    console.log(localStorage.getItem("userId"))
+
+    let pokemon1 = ['mewtwo-mega-y', 'zoroark']
+    let wishlist1 = ['scrafty', 'floette', 'shaymin-land']
+
+    const getPokemon = async () => {
+        await axios.get('/user-pokemon')
+    }
+
+    
+    if (localStorage.getItem("userId") === routeParams.id) {
+        return (
+            <div id="user-pokemon">
+                <h1 className="title">Your Pokemon</h1>
+                <div className="card-container">
+                    {                
+                        pokemon1.map(e => (<PokemonCard image={e} key={e}/>))
+                    }              
+                </div>
+                <h1 className="title">Looking For</h1>
+                <div className="card-container">
+                    {
+                        wishlist1.map(e => (<PokemonCard image={e} wish={true} key={e}/>))
+                    }
+                </div>
             </div>
-            <h1 className="title">Looking For</h1>
-            <div className="card-container">
-                {
-                    wishlist.map(e => (<PokemonCard image={e} wish={true} key={e}/>))
-                }
-            </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <h1 className='err-header'>Invalid User</h1>
+        )
+    }
 }
 
 export default UserPokemon
