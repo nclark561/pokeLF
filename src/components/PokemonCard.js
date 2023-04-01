656e75b1ddd0b03e715f4497134e6226c2035fc1
Noah Clark <noahammon00@gmail.com>
2023-04-01 11:05:53 -0600

    added non functional delete pokemon and edit pokemon buttons to each pokemon card

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -7,24 +7,30 @@ const PokemonCard = (props) => {
     const isWish = props.wish;
     
     return (
-        <div className="card-div">
-            <div>
-                <div className="image-div">
-                    <PokeSprite image={props.image} shiny={props.isShiny}/>
+        <div className="btn-div">
+            <div className="card-div">
+                <div>
+                    <div className="image-div">
+                        <PokeSprite image={props.image} shiny={props.isShiny}/>
+                    </div>
+                    <div className="type-div">
+                        <img src={`https://www.serebii.net/pokedex-bw/type/${props.type1}.gif`} alt='type1' className="type-image"/>
+                        {props.type2 ? <img src={`https://www.serebii.net/pokedex-bw/type/${props.type2}.gif`} alt='type2' className="type-image"/> : null}
+                    </div>
                 </div>
-                <div className="type-div">
-                    <img src={`https://www.serebii.net/pokedex-bw/type/${props.type1}.gif`} alt='type1' className="type-image"/>
-                    {props.type2 ? <img src={`https://www.serebii.net/pokedex-bw/type/${props.type2}.gif`} alt='type2' className="type-image"/> : null}
+                <div>
+                    <h3 className="card-h">#{props.pokedex} {pokeName}</h3>
+                    <ul className="card-list">
+                        <li>Nickname: {props.nickname ? props.nickname : 'no nickname'}</li>
+                        <li>Gender: {props.gender ? props.gender : null}</li>
+                        <li>Shiny: {props.isShiny ? 'yes' : 'no'}</li>
+                        {!isWish ? <li>For Trade: {props.forTrade ? 'yes' : 'no'}</li> : null}
+                    </ul>
                 </div>
             </div>
-            <div>
-                <h3 className="card-h">#{props.pokedex} {pokeName}</h3>
-                <ul className="card-list">
-                    <li>Nickname: {props.nickname ? props.nickname : 'no nickname'}</li>
-                    <li>Gender: {props.gender ? props.gender : null}</li>
-                    <li>Shiny: {props.isShiny ? 'yes' : 'no'}</li>
-                    {!isWish ? <li>For Trade: {props.forTrade ? 'yes' : 'no'}</li> : null}
-                </ul>
+            <div className="other-btn">
+            <button className="delete-btn">Delete Pokemon</button>
+            <button className="edit-btn">Edit Pokemon</button>
             </div>
         </div>
     )
